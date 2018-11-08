29e6e6f40c1a70421f7621a08918ccc195344a90
Reda Bouamira <reda-bouamira@outlook.fr>
2018-11-08 06:14:19 -0600

    Completed the request and response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PlayerIndex } from '@angular/core/src/render3/interfaces/player';
+import { HttpClient } from '@angular/common/http';
+import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +9,33 @@ import { PlayerIndex } from '@angular/core/src/render3/interfaces/player';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  response: any;
+  playersString;
+  winnerString;
   title = 'Poker';
+  players = Array();
 
-  players = [{ }];
+  // Adds a player to the players list
+  addPlayer(playerInfo){
+    var name = playerInfo.split(',', 1)[0];
+    var cards = playerInfo.split(',').slice(1).join(',').trim();
 
-  getRequestString = '';
-
-  addPlayer(name, cards){
     var newPlayer = {
       name: name,
       cards: cards
     };
 
     this.players.push(newPlayer);
-    this.getRequestString = JSON.stringify(this.players);
+    this.playersString = JSON.stringify(this.players);
+  }
+
+  constructor(private http: HttpClient) {}
+
+  // Sends a get request to the API and gets a response back.
+  getResponse(){
+    var playersJSON = '{ "Players" : ' + this.playersString + '}';
+    var requestString = 'http://localhost:64781/api/poker?play=' + playersJSON;
+    this.response = this.http.get(requestString);
+    this.winnerString = 'The winner is: ';
   }
 }
