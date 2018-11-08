da12ce2398f318d70626bcd6313d135f9281f68e
Reda Bouamira <reda-bouamira@outlook.fr>
2018-11-08 04:12:46 -0600

    UI fully setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlayerIndex } from '@angular/core/src/render3/interfaces/player';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'poker';
+  title = 'Poker';
+
+  players = [{ }];
+
+  getRequestString = '';
+
+  addPlayer(name, cards){
+    var newPlayer = {
+      name: name,
+      cards: cards
+    };
+
+    this.players.push(newPlayer);
+    this.getRequestString = JSON.stringify(this.players);
+  }
 }
